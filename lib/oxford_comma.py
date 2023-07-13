4abca210ba2574324493def92a57aa7ff919af57
RAHUL KHALLAR <khallarrahul@gmail.com>
2023-07-12 17:11:35 -0700

    resolves

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,14 @@
 def oxford_comma(items):
-    return None
+    if len(items) == 1:
+        print(items[0])
+        return items[0]
+    elif len(items) == 2:
+        print(" and ".join(items))
+        return " and ".join(items)
+    else:
+        joined_items = ", ".join(items[:-1]) + ", and " + items[-1]
+    print(joined_items)
+    return joined_items
+
+
+oxford_comma(["fiddleheads", "okra"])
